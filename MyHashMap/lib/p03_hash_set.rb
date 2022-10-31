b98e7fe6e3caace69bf2bc902d27839f56c9a131
chadfitz <fitzchad@gmail.com>
2022-10-31 15:27:40 -0700

    Implement HashSet

--- MyHashMap/lib/p03_hash_set.rb
+++ MyHashMap/lib/p03_hash_set.rb
@@ -7,18 +7,32 @@ class HashSet
   end
 
   def insert(key)
+    if include?(key)
+      return false
+    else
+      resize! if @count == num_buckets
+      self[key.hash] << key
+      @count += 1
+      return true
+    end
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
+  def [](key_hash)
     # optional but useful; return the bucket corresponding to `num`
+    @store[key_hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +40,12 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self[el.hash] << el
+      end
+    end
   end
 end
