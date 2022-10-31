54b20bcf293da7ac384981c2afc033d8c155a565
chadfitz <fitzchad@gmail.com>
2022-10-31 14:29:05 -0700

    Implement IntSet, ResizingIntSet

--- MyHashMap/lib/p01_int_set.rb
+++ MyHashMap/lib/p01_int_set.rb
@@ -42,18 +42,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num unless include?(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -70,18 +74,34 @@ class ResizingIntSet
   end
 
   def insert(num)
+    #check number of elements
+    #resize if we're at num_buckets + 1
+    if include?(num)
+      return false
+    else
+      resize! if @count == num_buckets
+      self[num] << num
+      @count += 1
+      return true
+    end
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -89,5 +109,12 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self[el] << el
+      end
+    end
   end
 end
