6b2adbf4ce3f57efb4516f6c08af41da8203bcaa
chadfitz <fitzchad@gmail.com>
2022-10-31 15:14:00 -0700

    p02 done

--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    res = 0
+    self.each_with_index do |el,i|
+      subres = el.hash + i.hash
+      res ^= subres
+    end
+    res
   end
 end
 
 class String
   def hash
+    res = 0
+    self.bytes.each.with_index do |el,i|
+      subres = el.hash + i.hash
+      res ^= subres
+    end
+    res
   end
 end
 
@@ -16,6 +28,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    res = 0
+    self.each do |k,v|
+      subres = k.hash + v.hash
+      res ^= subres
+    end
+    res
   end
 end
